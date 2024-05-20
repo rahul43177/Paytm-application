72c2353984f4b95759f7edcbafc57ffdeaec7255
Rahul <rahulmishra4317@gmail.com>
2024-05-21 01:10:11 +0530

    Home page

---
+++ frontend/src/components/HomeComponent.jsx
@@ -0,0 +1,7 @@
+export const HomeComponent = () => {
+    return (
+        <div className="flex items-center justify-center bg-red-500">
+            <h1 className="bg-white font-bold text center text-6xl">HOME PAGE</h1>
+        </div>
+    )
+}
