f3889ca6b0fa3527c750bf50ea460ba607217810
Rahul <rahulmishra4317@gmail.com>
2024-06-09 13:36:56 +0530

    payment modal is here

--- frontend/src/components/HomeComponent.jsx
+++ frontend/src/components/HomeComponent.jsx
@@ -1,23 +1,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Modal from 'react-modal'
+import "react-toastify/dist/ReactToastify.css";
+
+Modal.setAppElement('#root'); 
+
 export const HomeComponent = () => {
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
-const userName = localStorage.getItem("name");
-console.log("🚀 ~ HomeComponent ~ userName:", userName)
-const userEmail = localStorage.getItem("email")
-  console.log("🚀 ~ HomeComponent ~ userEmail:", userEmail)
+  const [modalIsOpen , setModalIsOpen] = useState(false);
+  const userName = localStorage.getItem("name");
+  const userEmail = localStorage.getItem("email");
   const handleLogout = async () => {
     try {
       const response = await axios.get(`http://localhost:3030/user/logout`, {
         withCredentials: true,
       });
       if (response.data.status == true) {
+        toast.success("The logout is successfull");
         window.location.reload();
       }
-      console.log(response);
-      console.log("The logout is done.");
     } catch (error) {
       console.log(error);
     }
@@ -30,28 +34,30 @@ const userEmail = localStorage.getItem("email")
   useEffect(() => {
     const getUsersList = async () => {
       try {
-        console.log("email of user----" , userEmail)
         const response = await axios.post(
-          "http://localhost:3030/user/usersList" , 
+          "http://localhost:3030/user/usersList",
           {
-            loggedInUser : userEmail 
-          } ,
+            loggedInUser: userEmail,
+          },
           {
-            withCredentials : true
+            withCredentials: true,
           }
         );
-        console.log(response);
+
         setUsers(response.data.userData);
-        console.log("data for users set in the useState");
       } catch (error) {
-        console.log(error);
         toast.error(error.response.data.message);
       }
     };
     getUsersList();
   }, []);
-  
-  console.log("users ----> ", users);
+
+  const openModal = () => setModalIsOpen(true)
+  const closeModal = () => setModalIsOpen(false)
+
+
+
+
   return (
     <>
       <div className="flex items-center justify-between p-4">
@@ -97,13 +103,41 @@ const userEmail = localStorage.getItem("email")
               <div className="flex-1 text-center text-red-700 font-semibold">
                 {user.email}
               </div>
-              <button className="flex-1 text-center bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-green-600 hover:font-bold transition duration-300">
+              <button className="flex-1 text-center bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-green-600 hover:font-bold transition duration-300" 
+              onClick={openModal}
+              >
                 Pay
               </button>
             </li>
           ))}
         </ul>
       </div>
+      <Modal 
+        isOpen={modalIsOpen} 
+        onRequestClose={closeModal} 
+        className="bg-white rounded-lg shadow-lg max-w-md mx-auto my-8 p-6 absolute pt-10"
+        overlayClassName="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center"
+      >
+        <h2 className="text-xl font-bold mb-4">Enter Payment Amount</h2>
+        <input 
+          type="text" 
+          placeholder="Enter the amount" 
+          className="w-full p-2 border border-gray-300 rounded mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <div className="flex justify-center space-x-4">
+        <button 
+          onClick={closeModal} 
+          className="absolute top-3 right-3 text-gray-600 hover:text-red-500 transition duration-300"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+        </button>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Pay
+          </button>
+        </div>
+      </Modal>
     </>
   );
 };
